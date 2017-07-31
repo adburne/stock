637006954a9a784f210d6788744a339656236358
adburne <adburne@hotmail.com>
2017-07-31 13:22:03 -0300

    Se hace referencia al reporte en formato pdf en vez de Word

--- core/app/view/products-view.php
+++ core/app/view/products-view.php
@@ -1,13 +1,13 @@
 <div class="row">
 	<div class="col-md-12">
 <div class="btn-group  pull-right">
-	<a href="index.php?view=newproduct" class="btn btn-default">Agregar Producto</a>
+    <a href="index.php?view=newproduct" class="btn btn-default"><i class='fa fa-paperclip'></i> Nuevo Producto</a>
 <div class="btn-group pull-right">
   <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
     <i class="fa fa-download"></i> Descargar <span class="caret"></span>
   </button>
   <ul class="dropdown-menu" role="menu">
-    <li><a href="report/products-word.php">Word 2007 (.docx)</a></li>
+    <li><a href="report/products-pdf.php">Reporte (.pdf)</a></li>
   </ul>
 </div>
 </div>
